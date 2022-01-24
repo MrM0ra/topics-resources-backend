72485010b481c2e963cf853daa0828c499ec3dc6
Victor Esteban Mora Arce <themonstermora@gmail.com>
2022-01-24 10:29:31 -0500

    Resources rest controller implemented

--- src/main/java/com/topics/resources/backend/rest/TopicController.java
+++ src/main/java/com/topics/resources/backend/rest/TopicController.java
@@ -18,27 +18,48 @@ import com.topics.resources.backend.services.implementation.TopicServiceImplemen
  * @author victor.mora
  *
  */
+
 @RestController
 public class TopicController {
 
 	@Autowired
 	private TopicServiceImplementation topicService;
 	
+	/**
+	 * Returns every single Topic on the db
+	 * @return all the topics in the db
+	 */
 	@GetMapping("/topics")
 	private List<Topic> getTopics() {
 		return topicService.findAllTopics();
 	}
 	
+	/**
+	 * Returns a Topic with the specified id
+	 * @param topicId the id of the Topic
+	 * @return Topic with the specific id, null if it doesn't exists
+	 */
 	@GetMapping("/topics/{topicId}")
 	private Topic getTopic(@PathVariable long topicId) {
 		return topicService.findTopicById(topicId);
 	}
 	
+	/**
+	 * Adds a new topic to the db
+	 * @param topic the topic to be added to the database
+	 * @return the created new topic on the database
+	 */
 	@PostMapping("/topics")
 	private Topic addTopics(@RequestBody Topic topic) {
 		return topicService.createTopic(topic);
 	}
 	
+	/**
+	 * Edits an existent topic from the database
+	 * @param topicId the id of the topic to be modified
+	 * @param topic a topic with the new information to insert into the modifying topic
+	 * @return the modified topic, null if the id doesn't exists
+	 */
 	@PutMapping("/topics/{topicId}")
 	private Topic getTopic(@PathVariable long topicId, @RequestBody Topic topic) {
 		return topicService.editTopic(topicId, topic);
